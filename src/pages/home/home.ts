1e3977880a2c0fabab020d48acbebcc739d29b30
Andres <andresdev@gmail.com>
2018-12-02 14:03:01 -0500

    add list with playing audio snippets

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ANIMALES } from '../../data/data.animales';
+
+import { Animal } from '../../interfaces/animal.interface';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  animales: Animal[] = [];
 
   constructor(public navCtrl: NavController) {
 
+      this.animales = ANIMALES.splice(0);
+  
+    }
+
+  reproducir(animal) {
+      console.log(animal);
+
+      let audio = new Audio();
+      audio.src = animal.audio;
+
+      audio.load();
+      audio.play();
+      animal.reproduciendo = true;
+
+      setTimeout( ()=> animal.reproduciendo = false, animal.duracion * 1000);
   }
 
 }
